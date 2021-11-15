b8b72c553048550cf5a871497c194b2b5061c5d9
thre-huishou <2721061625@qq.com>
2021-11-15 14:33:54 +0800

    拷贝和替换

---
+++ STL-拷贝与替换/拷贝与替换/拷贝与替换/111.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+
+class myprint
+{
+public:
+	void operator()(int val)
+	{
+		cout << val << " ";
+	}
+};
+//copy
+int main()
+{
+	vector<int> v1;
+	for (int i = 0; i < 10; i++)
+	{
+		v1.push_back(i);
+	}
+	for_each(v1.begin(), v1.end(), myprint());
+	cout << endl;
+	vector<int> v2;
+	v2.resize(v1.size());
+	copy(v1.begin(), v1.end(), v2.begin());
+	for_each(v2.begin(), v2.end(), myprint());
+	cout << endl;
+	return 0;
+}
